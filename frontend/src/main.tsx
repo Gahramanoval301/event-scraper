44dd73dd728c193d9e0171949f8ea3a8c71f129a
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-02-08 22:52:01 +0400

    add all your wrappers like suspense, browser router and themeprovider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router'
+import Loading from './components/Loading.tsx'
+import { ThemeProvider } from '@mui/material'
+import theme from '../theme'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <Suspense fallback={<Loading />}>
+          <App />
+        </Suspense>
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
