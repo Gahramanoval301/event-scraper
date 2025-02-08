daa445527af7d8d45ed164ca14edf61bf303e8aa
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-02-08 22:55:42 +0400

    returning sections for home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import PageLayout from '../components/_layout'
+import AuSydneyEvents from '../components/_home/AuSydneyEvents'
+
+const Home = () => {
+  return (
+    <PageLayout>
+      <AuSydneyEvents/>
+    </PageLayout>
+  )
+}
+
+export default Home
