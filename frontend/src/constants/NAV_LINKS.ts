f0294b9fdf76f7f2969c75e83c1103af7661c1c7
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-02-08 22:55:19 +0400

    constant nav links array is added

---
+++ frontend/src/constants/NAV_LINKS.ts
@@ -0,0 +1,12 @@
+export const NAV_LINKS = [
+  {
+    id: 0,
+    label: "Home",
+    to: "/",
+  },
+  {
+    id: 1,
+    label: "Github",
+    to: "https://github.com/Gahramanoval301/event-scraper",
+  },
+];
